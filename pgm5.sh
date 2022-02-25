ec8ede64660ccca5e642cccb15f424f2aa720f4e
rithesh-gowda <100407956+rithesh-gowda@users.noreply.github.com>
2022-02-25 15:44:39 +0530

    Update pgm5.sh

--- pgm5.sh
+++ pgm5.sh
@@ -1,4 +1,4 @@
-Write a shell script that accepts a path name and creats all the comparision in that path name as directories.for example if the script is named as mpc,then the a/b/c/d create sub directory a,a/b,a/b/c,a/b/c/d.
+#Write a shell script that accepts a path name and creats all the comparision in that path name as directories.for example if the script is named as mpc,then the a/b/c/d create sub directory a,a/b,a/b/c,a/b/c/d.
 if [ $# -ne 0 ]
 then
 p=`echo $1 | tr "/" " "`
