bd1fe26e5061327b604fb28ed4972a1668f69544
rithesh-gowda <100407956+rithesh-gowda@users.noreply.github.com>
2022-02-25 15:28:35 +0530

    Update pgm5.sh

--- pgm5.sh
+++ pgm5.sh
@@ -1,3 +1,4 @@
+Write a shell script that accepts a path name and creats all the comparision in that path name as directories.for example if the script is named as mpc
 if [ $# -ne 0 ]
 then
 p=`echo $1 | tr "/" " "`
